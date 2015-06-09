4b17249b5a9d668b718237c10643dd99e035b01c
Erkepucko <filip.borg@hotmail.se>
2015-06-09 20:55:59 +0200

    As it was submitted

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 function openRoom(room, lang) {
     var iframe = document.getElementById("iframe");
     if (lang === 'sv') {
-        var src = "/src/pannellum.htm?tour=/pano/svPano.json&firstScene=" + room + "&";
+        var src = "src/pannellum.htm?tour=/pano/svPano.json&firstScene=" + room + "&";
     } else if (lang === 'en' || lang === 'eng') {
         var src = "/src/pannellum.htm?tour=/pano/enPano.json&firstScene=" + room + "&";
     }
@@ -15,30 +15,7 @@ function openRoom(room, lang) {
 function closeRoom() {
     document.getElementById("iframe").style.display = "none";
     document.getElementById("closeButton").style.display = "none";
-    document.getElementById("mainContent").style.background = "url('/img/icons/map.svg')";
+    document.getElementById("mainContent").style.background = "url('img/icons/map.svg')";
     document.getElementById("icons").style.display = "block";
+    document.getElementById("iframe").setAttribute("src", "");
 }
-
-
-
-
-
-
-
-
-
-        /*function loadXMLDoc() {
-            var xmlhttp;
-            if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
-                xmlhttp = new XMLHttpRequest();
-            } else { // code for IE6, IE5
-                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            xmlhttp.onreadystatechange = function () {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-                }
-            }
-            xmlhttp.open("GET", "/learnAjax/txt.txt", true);
-            xmlhttp.send();
-        }*/
